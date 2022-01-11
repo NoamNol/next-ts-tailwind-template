59ca989bb923fd83414394e620d5181bf57c1822
NoamNol <noamnol19@gmail.com>
2022-01-11 18:08:06 +0200

    Add eslint rules and fix lint problems

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable react/jsx-props-no-spreading */
 
-function MyApp({ Component, pageProps }: AppProps) {
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
