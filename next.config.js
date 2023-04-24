6e90e50834815ab715cabe9b6492200df98636f7
Noam Nol <noamnol19@gmail.com>
2023-04-24 22:22:38 +0300

    Move page files to app folder

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
   reactStrictMode: true,
   // Create a standalone folder which copies only the necessary files for production
   output: 'standalone',
+  experimental: {
+    appDir: true,
+  }
 }
