3b6dc5ce6e662169d4a35c8eb99cf52dabb2f75b
Noam Nol <noamnol19@gmail.com>
2023-04-24 19:33:48 +0300

    'outputStandalone' is no longer experimental

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    // Create a standalone folder which copies only the necessary files for production
-    outputStandalone: true,
-  },
+  // Create a standalone folder which copies only the necessary files for production
+  output: 'standalone',
 }
