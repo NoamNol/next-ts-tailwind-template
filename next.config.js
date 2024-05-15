f59fddc99ce0f57ce40bd3b569b3db844ce6f3c0
Noam Nol <noamnol19@gmail.com>
2024-05-15 22:05:12 +0300

    Improve Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // Create a standalone folder which copies only the necessary files for production
+  // Create a standalone folder which copies only the necessary files for production. This is necessary for Docker image
   output: 'standalone',
   experimental: {
     appDir: true,
