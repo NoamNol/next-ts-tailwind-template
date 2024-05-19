e9c98521f52df4b37c9f4f5f7ff07e66a0420b94
Noam Nol <noamnol19@gmail.com>
2024-05-20 00:26:55 +0300

    fix(PlayWithActiveLink): useSearchParams must be in Suspense

--- src/components/PlayWithActiveLink/index.tsx
+++ src/components/PlayWithActiveLink/index.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 import { ActiveLink } from '@/components/ActiveLink'
-
 import type React from 'react'
 
-export const PlayWithActiveLink: React.FC<{ className: string }> = ({ className }) => {
+const InnerPlayWithActiveLink: React.FC<{ className: string }> = ({ className }) => {
   const searchParams = useSearchParams()
   const initialIsActive = searchParams.get('active')
   const isActive = initialIsActive === null || initialIsActive === 'true'
@@ -19,5 +19,13 @@ export const PlayWithActiveLink: React.FC<{ className: string }> = ({ className 
         Test ActiveLink
       </ActiveLink>
     </div>
+  )
+}
+
+export const PlayWithActiveLink: React.FC<{ className: string }> = ({ className }) => {
+  return (
+    <Suspense>
+      <InnerPlayWithActiveLink className={className} />
+    </Suspense>
   )
 }
